628088d93da32c8f53f7c5ceb2cba17042a1b60b
reemals <reemals@my.yorku.ca>
2021-08-13 18:15:05 +0000

    added shares/SYMBOL

--- SampleSpringBoot/src/main/java/com/citi/training/SampleSpringBoot/service/SharesService.java
+++ SampleSpringBoot/src/main/java/com/citi/training/SampleSpringBoot/service/SharesService.java
@@ -1,13 +1,11 @@
 package com.citi.training.SampleSpringBoot.service;
-
 import com.citi.training.SampleSpringBoot.entities.Shares;
 import org.springframework.stereotype.Service;
-
 import java.util.Collection;
-
 @Service
 public interface SharesService {
     Collection<Shares> getAllShares();
-
-
+    Collection<Shares>  getShareBySymbol (String symbol);
+    Collection<Shares>  addNewShare(Shares share);
+    void SellShares(String symbol);
 }
--- SampleSpringBoot/src/main/java/com/citi/training/SampleSpringBoot/service/SharesServiceImpl.java
+++ SampleSpringBoot/src/main/java/com/citi/training/SampleSpringBoot/service/SharesServiceImpl.java
@@ -1,21 +1,26 @@
 package com.citi.training.SampleSpringBoot.service;
-
 import com.citi.training.SampleSpringBoot.entities.Shares;
 import com.citi.training.SampleSpringBoot.repo.SharesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.Collection;
-
 @Service
 public class SharesServiceImpl implements SharesService {
-
     @Autowired
     private SharesRepository sharesRepository;
-
     @Override
     public Collection<Shares> getAllShares() {
         return sharesRepository.findAll();
     }
-
+    @Override
+    public Collection<Shares> getShareBySymbol (String symbol) {
+        return sharesRepository.findBySymbol(symbol);
+    }
+    @Override
+    public Collection<Shares> addNewShare(Shares share) {
+        return null;
+    }
+    @Override
+    public void SellShares(String symbol) {
+    }
 }
